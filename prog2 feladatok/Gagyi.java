ac4a59a486412852a53e8b362d561ec8d86b4d94
SylwerStone <43640947+SylwerStone@users.noreply.github.com>
2020-09-30 12:08:40 +0200

    Add files via upload

---
+++ prog2 feladatok/Gagyi.java
@@ -0,0 +1,11 @@
+class Gagyi
+{
+	public static void main(String[] args)
+	{
+		Integer t = 127;
+		Integer x = 127;
+		
+		while(x <= t && x>=t && t != x)
+			System.out.println("hop");		 
+	}
+}      
